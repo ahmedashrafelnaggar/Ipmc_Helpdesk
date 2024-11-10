79f07b6a87c8d2f6d3604a48471d5276ecedc415
ahmedashraf <www.ahmedashraf83@gmail.com>
2024-11-10 11:39:51 +0200

    Ipmc Helpdesk

--- ipmc_helpdesk_ticket/models/hr_employee.py
+++ ipmc_helpdesk_ticket/models/hr_employee.py
@@ -4,6 +4,9 @@ class HrEmployee(models.Model):
     _inherit = 'hr.employee'
     _description = 'Employee'
 
+    certificate_id = fields.Many2one('ipmc.certificates', string="Certificate",help="The certificate associated with this degree")
+
+
 
 
 
